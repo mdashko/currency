71b1e7bbe9ae965a6e2111f8948058348f872621
Марта <marta@MacBook-Air-Marta.local>
2023-04-12 18:41:16 +0300

    fetch data from api

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./scss/App.scss";
+import { CurrencyTable } from "./components/CurrencyTable";
+function App() {
+	return (
+		<div className="App">
+			<CurrencyTable />
+		</div>
+	);
+}
+
+export default App;
